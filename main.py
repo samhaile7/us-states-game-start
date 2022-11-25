a69f6c135d778bf8b59b482b1e235a9db85d56bb
Samuel Haile <samhaile129@gmail.com>
2022-11-24 22:59:06 -0500

    checking series for match to user input

--- main.py
+++ main.py
@@ -7,11 +7,19 @@ img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-answer = screen.textinput(title= "Guess the state!", prompt= "What's the state name?")
-print(answer)
+while game_is_on:
+    answer = screen.textinput(title= "Guess the state!", prompt= "What's the state name?")
+    print(answer)
 
-data = pandas.read_csv("50_states.csv")
-print(data)
-row = data[data.state == answer]
+    data = pandas.read_csv("50_states.csv")
+    print(data)
+    column_list = data['state'].to_list
+    for state in column_list:
+        if answer == state:
+            row = data[data.state == answer]
+            #Print name on map here
+        else:
+            print('try again')
+
 print(row)
 
